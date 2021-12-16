d535dd9baf74d86f9508ecd409dfbfcc81444aec
Ani Dotan <ani.dotan97@gmail.com>
2021-12-16 19:20:42 +0200

    temp commit

---
+++ game.py
@@ -0,0 +1,32 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def initialize_game(self, snake_loc: ):
+        """
+        1. place the snake
+        2. place the bombs
+        3. place the apples
+        4. call end_round ?
+        :return:
+        """
+
+    def single_round(self):
+        """
+        1. get input
+        2. move snake
+        4. check if there is a bomb explosion -> erase apples in the area
+        3. check if game ended
+
+        4. check if snake ate apple -> progress + update score
+        5. update apples + bombs for the next round
+        6. draw board
+        7. end round
+
+        :return:
+        """
+        # List[Tuple[int,int]], List[List[Tuple[int,int]]]
+        #  R, O
+
+    def update_score(self):
+        pass
